dea1cfd4c689f8ee72fcc0adaa721e44dcad6d5a
Vagner do Carmo <vluzrmos@gmail.com>
2015-05-10 11:28:06 -0300

    minifyed all

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
         <link rel="icon" href="{{$team['icon']['image_132']}}"/>
     @endif
 
-    @if(app()->environment('local'))
+    @if(app()->environment()=='local')
         <script type="text/javascript" src="{{url('js/jquery-2.1.4.min.js')}}"></script>
         <script type="text/javascript" src="{{url('js/socket.io-1.3.5.js')}}"></script>
 
@@ -21,40 +21,44 @@
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="{{url('js/bootstrap.min.js')}}"></script>
+        <link rel="stylesheet" href="{{url('css/app.css')}}"/>
+
+        <script type="text/javascript" src="{{url('js/messages.js')}}"></script>
+        <script type="text/javascript">
+            var app = app||{};
+            app.config = app.config||{};
+
+            app.config.debug = Number.parseInt('{{env('APP_DEBUG')}}');
+            app.config.websocket = {
+                host: '{{env('WS_HOST')}}'||window.location.hostname,
+                port: '{{env('WS_PORT')}}'||'8080'
+            };
+        </script>
+        <script type="text/javascript">Lang.setLocale('{{config('app.locale')}}');</script>
+        <script type="text/javascript" src="{{url('js/app.js')}}"></script>
     @else
-        <script type="text/javascript" src="//code.jquery.com/jquery-2.1.4.min.js"></script>
-        <script type="text/javascript" src="//cdn.socket.io/socket.io-1.3.5.js"></script>
+        <link rel="stylesheet" href="{{url('css/all.css')}}"/>
 
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <script type="text/javascript" src="{{url('js/vendor.js')}}"></script>
 
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+        <script type="text/javascript">
+            var app = app||{};
+            app.config = app.config||{};
 
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+            app.config.debug = Number.parseInt('{{env('APP_DEBUG')}}');
+            app.config.websocket = {
+                host: '{{env('WS_HOST')}}'||window.location.hostname,
+                port: '{{env('WS_PORT')}}'||'8080'
+            };
+        </script>
+        <script type="text/javascript">Lang.setLocale('{{config('app.locale')}}');</script>
+        <script type="javascript" src="{{url('js/app.min.js')}}"></script>
     @endif
-
-    <link rel="stylesheet" href="{{url('css/app.css')}}"/>
-
-    <script type="text/javascript" src="{{url('js/messages.js')}}"></script>
-    <script type="text/javascript">
-        var app = app||{};
-        app.config = app.config||{};
-
-        app.config.debug = Number.parseInt('{{env('APP_DEBUG')}}');
-        app.config.websocket = {
-            host: '{{env('WS_HOST')}}'||window.location.hostname,
-            port: '{{env('WS_PORT')}}'||'8080'
-        };
-    </script>
-    <script type="text/javascript">Lang.setLocale('{{config('app.locale')}}');</script>
-    <script type="text/javascript" src="{{url('js/app.js')}}"></script>
-
 </head>
 <body>
+<div class="container">
     @yield('content')
-
+</div>
     <div id="footer">
         {!!trans('messages.copyright')!!}
     </div>
--- resources/views/slack/index.blade.php
+++ resources/views/slack/index.blade.php
@@ -1,48 +1,44 @@
 @extends('app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-sm-8 col-sm-offset-2" >
-            <div id="logo">
-                <img src="{{$team['icon']['image_132']}}" width="48"/>
-            </div>
+<div class="row">
+    <div class="col-sm-8 col-sm-offset-2" >
+        <div id="logo">
+            <img src="{{$team['icon']['image_132']}}" width="48"/>
+        </div>
 
-            <div id="message">
-                {!! trans('slackin.join', ['team' => $team['name']]) !!}
-            </div>
+        <div id="message">
+            {!! trans('slackin.join', ['team' => $team['name']]) !!}
+        </div>
 
-            <div id="status" >
-                {!! trans_choice('slackin.users_online', $totals['active'], $totals) !!}
-            </div>
+        <div id="status" >
+            {!! trans_choice('slackin.users_online', $totals['active'], $totals) !!}
+        </div>
 
-            <div id="form-invite" class="col-sm-6 col-sm-offset-3">
-                <div id="validation-message">
+        <div id="form-invite" class="col-sm-6 col-sm-offset-3">
+            <div id="validation-message"></div>
 
+            <form action="{{url('/invite')}}" method="post" class="form-horizontal" role="form" onsubmit="return false">
+                <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+
+                <div class="form-group">
+                    <input class="form-control input-sm" type="text" name="username" placeholder="{{trans('slackin.placeholders.username')}}" autofocus="true"/>
+                    <div class="help-block">
+
+                    </div>
                 </div>
 
-                <form action="{{url('/invite')}}" method="post" class="form-horizontal" role="form" onsubmit="return false">
-                    <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+                <div class="form-group">
+                    <input class="form-control input-sm" type="text" name="email" placeholder="{{trans('slackin.placeholders.email')}}"/>
+                    <div class="help-block">
 
-                    <div class="form-group">
-                        <input class="form-control input-sm" type="text" name="username" placeholder="{{trans('slackin.placeholders.username')}}" autofocus="true"/>
-                        <div class="help-block">
-
-                        </div>
                     </div>
+                </div>
 
-                    <div class="form-group">
-                        <input class="form-control input-sm" type="text" name="email" placeholder="{{trans('slackin.placeholders.email')}}"/>
-                        <div class="help-block">
-
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <input type="submit" class="form-control input-sm btn btn-default" value="{{trans('slackin.submit')}}"/>
-                    </div>
-                </form>
-            </div>
+                <div class="form-group">
+                    <input type="submit" class="form-control input-sm btn btn-default" value="{{trans('slackin.submit')}}"/>
+                </div>
+            </form>
         </div>
     </div>
 </div>
