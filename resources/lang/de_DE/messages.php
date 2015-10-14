79ede03c1691062e941fc19d9f8ec631ed546d52
2ndkauboy <bernhard@kau-boys.de>
2015-10-15 00:24:37 +0200

    adding German translation

---
+++ resources/lang/de_DE/messages.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'copyright' => 'Mit Liebe gemacht <i class="glyphicon glyphicon-heart" style="color: red;"></i> von <a href="https://github.com/vluzrmos" target="_blank">Vluzrmos</a> + <a href="http://lumen.laravel.com" target="_blank">Lumen Framework</a> - Fork auf <a href="https://github.com/vluzrmos/lumen-slackin" target="_blank">Github</a>.',
+];
